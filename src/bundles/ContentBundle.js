e00afb93341197f154abe8b6fb08920fe97cc33b
omostepaniuk <o.mostepaniuk@gmail.com>
2021-09-08 21:53:58 +0300

    Convert ContentBundle to a functional component

--- src/bundles/ContentBundle.js
+++ src/bundles/ContentBundle.js
@@ -2,31 +2,13 @@ import React from 'react';
 import  './ContentBundle.scss';
 import ContentChunks from './ContentChunks';
 
-class ContentBundle extends React.Component {
-
-  constructor(props, context) {
-    super(props, context);
-
-    this.state = {
-      isExpanded: false
-    };
-
-    this.toggleBundle = this.toggleBundle.bind(this);
-  }
-
-  toggleBundle() {
-    this.setState((state, props) => ({ isExpanded: !state.isExpanded }))
-  }
-
-  render() {
-    return (
-      <div className={`bundle-item`}>
-        <h1 className={`bundle-name ${this.state.isExpanded ? 'expanded' : ''}`}
-            onClick={this.toggleBundle}>{this.props.bundle.name}</h1>
-        <ContentChunks bundle={this.props.bundle}/>
-      </div>
-    );
-  }
+const ContentBundle = (props) => {
+  return (
+    <div className={`bundle-item`}>
+      <h1 className={`bundle-name`}>{props.bundle.name}</h1>
+      <ContentChunks bundle={props.bundle}/>
+    </div>
+  )
 }
 
 export default ContentBundle;
